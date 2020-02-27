70890c3eaffd890bffa71b01c91d3b7347cdfe50
Salasc2 <salasch2@gmail.com>
2020-02-26 20:07:10 -0800

    Create a base index template for website

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models');
 
-
 router.get('/', function(req, res, next) {
   res.render('index');
 });
