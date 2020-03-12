5baf62c85ad6225abb6fe2e1bd85339c5688df8d
Francis Li <francisli@users.noreply.github.com>
2020-03-11 20:17:53 -0700

    Support uploading a photo for an item (#6)

--- models/item.js
+++ models/item.js
@@ -3,8 +3,6 @@ module.exports = (sequelize, DataTypes) => {
   const Item = sequelize.define('Item', {
     photo: DataTypes.STRING,
     bin: DataTypes.INTEGER,
-    categoryId: DataTypes.INTEGER,
-    restaurantId: DataTypes.INTEGER
   }, {});
   Item.associate = function(models) {
     // associations can be defined here
--- routes/item.js
+++ routes/item.js
@@ -3,51 +3,52 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../models');
+const helpers = require('./helpers');
 
 router.get('/', function(req, res, next) {
-    models.Item.findAll().then(function(records) {
-    res.render('item/item', {
-        records: records
-      });
+  models.Item.findAll().then(function(records) {
+    res.render('item/index', {
+      records: records
+    });
   });
 });
 
-  router.get('/new', function(req, res, next) {
-    res.render('item/new');
+router.get('/new', function(req, res, next) {
+  res.render('item/new');
 });
 
-router.post('/', function(req, res, next) { 
+router.post('/', function(req, res, next) {
   models.Item.create({
     bin: req.body.bin,
-    restaurantId: req.body.restaurantId,
-    categoryId: req.body.categoryId,
+    RestaurantId: req.body.restaurantId,
+    CategoryId: req.body.categoryId,
     photo: req.body.photo,
   }).then(function(record) {
     res.redirect(`/item`);
   });
-
 });
 
 router.get('/:id', function(req, res, next) {
-    models.Item.findByPk(req.params.id).then(function(record) {
-      res.render('item/edit', {
-        record: record
-      });
+  models.Item.findByPk(req.params.id).then(function(record) {
+    res.render('item/edit', {
+      record: record
+    });
   });
 });
 
-  router.post('/:id', function(req, res, next) {
+router.post('/:id', function(req, res, next) {
   models.Item.findByPk(req.params.id).then(function(record) {
-    record.update({
-      bin: req.body.bin,
-      restaurantId: req.body.restaurantId,
-      categoryId: req.body.categoryId,
-      photo: req.body.photo,
-    }).then(function(record) {
-      res.redirect('/item');
-    })
+    helpers.handleUpload(record, 'photo', req.body.photo, 'items/photo').then(function(record) {
+      record.update({
+        bin: req.body.bin,
+        RestaurantId: req.body.restaurantId,
+        CategoryId: req.body.categoryId,
+        photo: record.photo,
+      }).then(function(record) {
+        res.redirect('/item');
+      })
+    });
   })
 });
 
-
-  module.exports = router;
+module.exports = router;
