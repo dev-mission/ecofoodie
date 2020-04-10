e6672aafccf1ebc790ea6399dbc752a6b7ce4b34
Francis Li <mail@francisli.com>
2020-04-10 16:21:31 -0700

    Link up items to restaurants

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -41,18 +41,67 @@ router.post('/', function(req, res, next){
 
 // Only for viewing a restaurant edit
 router.get('/:id/edit', function(req, res, next){
-  models.Restaurant.findByPk(req.params.id).then(function(record){
+  models.Restaurant.findByPk(req.params.id).then(function(record) {
     res.render('restaurants/edit',{
-      record : record
+      record
     });
+  });
+});
+
+router.get('/:id/categories/:categoryId/items', function(req, res, next) {
+  models.Restaurant.findByPk(req.params.id).then(function(restaurant) {
+    models.Category.findByPk(req.params.categoryId).then(function(category) {
+      models.Item.findAll({where: {RestaurantId: restaurant.id, CategoryId: category.id}}).then(function(items) {
+        res.render('restaurants/items/index',{
+          restaurant,
+          category,
+          items
+        });
+      });
+    });
+  });
+});
+
+router.get('/:id/items/new', function(req, res, next) {
+  models.Restaurant.findByPk(req.params.id).then(function(restaurant) {
+    models.Category.findAll().then(function(categories) {
+      res.render('restaurants/items/new',{
+        restaurant,
+        categories,
+        categoryId: req.query.categoryId
+      });
+    });
+  });
+});
+
+router.post('/:id/items', function(req, res, next) {
+  const record = models.Item.build({
+    bin: req.body.bin,
+    RestaurantId: req.params.id,
+    CategoryId: req.body.CategoryId
+  });
+  helpers.handleUpload(record, 'photo', req.body.photo, 'items/photo').then(function(record) {
+    record.save().then(function(record) {
+      res.redirect(`/restaurants/${req.params.id}/categories/${record.CategoryId}/items`);
+    })
   });
 });
 
 // Only for viewing a restaurant edit
 router.get('/:id', function(req, res, next){
   models.Restaurant.findByPk(req.params.id).then(function(record){
-    res.render('restaurants/show',{
-      record : record
+    models.sequelize.query(`
+      SELECT *
+      FROM "Categories"
+      WHERE id IN
+      (SELECT DISTINCT("CategoryId")
+       FROM "Items"
+       WHERE "RestaurantId"=${req.params.id})
+    `, {model: models.Category}).then(function(categories) {
+      res.render('restaurants/show',{
+        record: record,
+        categories: categories
+      });
     });
   });
 });
